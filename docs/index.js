c378627bc5cfa910fa84abcc967c34b642a49338
Mateusz Karpinski <mateusz.karpinski29@gmail.com>
2024-08-05 19:35:34 +0200

    initial commit

---
+++ docs/index.js
@@ -0,0 +1,20 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return(title + " by " + author + ", " + pages + ", " + read);
+  }
+}
+
+function addBookToLibrary() {
+  
+}
+
+
+// test
+// const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295', 'not');
+// console.log(theHobbit.info());
