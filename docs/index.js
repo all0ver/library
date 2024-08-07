078a2a5d2bf582c100f4936032a2e6d34338845c
Mateusz Karpinski <mateusz.karpinski29@gmail.com>
2024-08-07 19:11:03 +0200

    added top-bar with styles

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,11 @@
+const addBtn = document.querySelector("#addBtn");
+const allBtn = document.querySelector("#all");
+
 const myLibrary = [];
+
+function listAll() {
+  myLibrary.forEach((book) => console.log(book.title));
+}
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -10,10 +17,21 @@ function Book(title, author, pages, read) {
   }
 }
 
-function addBookToLibrary() {
-  
+// elements from html
+
+
+function addBookToLibrary(sTitle, sAuthor, sPages, sRead) {
+  myLibrary.push(new Book(sTitle, sAuthor, sPages, sRead));
 }
 
+addBtn.addEventListener("click", () => {
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const read = document.querySelector("#read").checked;
+  addBookToLibrary(title, author, pages, read);
+});
+allBtn.addEventListener("click", listAll);
 
 // test
 // const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295', 'not');
