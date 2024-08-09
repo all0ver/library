dc267a97d8f0aec9e2d281617cb5a90877aae527
Mateusz Karpinski <mateusz.karpinski29@gmail.com>
2024-08-09 00:27:43 +0000

    added delete funcionality

--- docs/index.js
+++ docs/index.js
@@ -3,6 +3,12 @@ const allBtn = document.querySelector("#all");
 
 const myLibrary = [];
 
+myLibrary.forEach((book) => {
+  addElement(book.title, book.author, book.pages, book.read);
+});
+
+// const titles = myLibrary.map(Book => Book.title);
+
 function listAll() {
   myLibrary.forEach((book) => console.log(book.title));
 }
@@ -19,11 +25,8 @@ function Book(title, author, pages, read) {
 
 // elements from html
 
-function addBookToLibrary(sTitle, sAuthor, sPages, sRead) {
-  myLibrary.push(new Book(sTitle, sAuthor, sPages, sRead));
-}
-
 function addElement(sTitle, sAuthor, sPages, sRead) {
+  myLibrary.push(new Book(sTitle, sAuthor, sPages, sRead));
   const main = document.querySelector("#main");
   const div = document.createElement("div");
   const divClasses = [
@@ -57,28 +60,42 @@ function addElement(sTitle, sAuthor, sPages, sRead) {
   });
 
   const divButton = document.createElement("button");
-  const buttonClasses = ['custom-button','place-self-end', 'bg-red-300', 'flex', 'justify-center', 'items-center', 'w-10', 'h-10'];
+  const buttonClasses = [
+    "custom-button",
+    "place-self-end",
+    "bg-red-300",
+    "flex",
+    "justify-center",
+    "items-center",
+    "w-10",
+    "h-10",
+  ];
   addManyClasses(divButton, buttonClasses);
-  divButton.addEventListener('click', () => {
-    console.log("eoeo");
-    //TODO:
-    //div deleting + you have to delete from the array on top
-  })
+  divButton.addEventListener("click", () => {
+    let index = myLibrary.findIndex(
+      (book) => book.title == divButton.parentNode.firstChild.textContent,
+    );
+    if (index > -1) {
+      myLibrary.splice(index, 1);
+    }
+    console.log(myLibrary);
+    divButton.parentNode.remove();
+  });
   const divChildren = [divTitle, divAuthor, divPages, divStatus, divButton];
-  
+
   // adding text to elements
   divTitle.textContent = sTitle;
-  divAuthor.innerHTML = "Author: <span class='font-normal'>"+sAuthor+"</span>";
-  divPages.innerHTML = "Pages: <span class='font-normal'>"+sPages+"</span>";
-  divStatus.innerHTML = "Status: <span class='font-normal'>"+sRead+"</span>";
+  divAuthor.innerHTML =
+    "Author: <span class='font-normal'>" + sAuthor + "</span>";
+  divPages.innerHTML = "Pages: <span class='font-normal'>" + sPages + "</span>";
+  divStatus.innerHTML =
+    "Status: <span class='font-normal'>" + sRead + "</span>";
   divButton.textContent = "D";
   divChildren.forEach((child) => {
     div.appendChild(child);
   });
 
   main.appendChild(div);
-  
-  
 }
 
 function addManyClasses(tag, list) {
@@ -87,34 +104,29 @@ function addManyClasses(tag, list) {
   });
 }
 
-addElement("text", "autor 123", "300", "not");
-addElement("text", "autor 123", "300", "not");
-addElement("text", "autor 123", "300", "not");
-addElement("text", "autor 123", "300", "not");
-addElement("text", "autor 123", "300", "not");
-addElement("text", "autor 123", "300", "not");
-addElement("text", "autor 123", "300", "not");
-addElement("text", "autor 123", "300", "not");
-addElement("text", "autor 123", "300", "not");
-addElement("text", "autor 123", "300", "not");
-addElement("text", "autor 123", "300", "not");
-addElement("text", "autor 123", "300", "not");
-addElement("text", "autor 123", "300", "not");
-addElement("text", "autor 123", "300", "not");
-addElement("text", "autor 123", "300", "not");
-addElement("text", "autor 123", "300", "not");
-addElement("text", "autor 123", "300", "not");
-
 // addBtn.addEventListener("click", () => {
 //   const title = document.querySelector("#title").value;
 //   const author = document.querySelector("#author").value;
 //   const pages = document.querySelector("#pages").value;
 //   const read = document.querySelector("#read").checked;
-//   addBookToLibrary(title, author, pages, read);
+//   addElement(title, author, pages, read);
 // });
 // allBtn.addEventListener("click", listAll);
-
-// test
-// const theHobbit = new Book('The Hobbit', 'J.R.R Tolkien', '295', 'not');
-// console.log(theHobbit.info());
-
+//
+addElement("The Hobbit", "J.R.R. Tolkien", 310, "not read");
+addElement("1984", "George Orwell", 328, "read");
+addElement("Test Book", "Author Test", 123, "read");
+addElement("To Kill a Mockingbird", "Harper Lee", 336, "read");
+addElement("The Great Gatsby", "F. Scott Fitzgerald", 180, "read");
+addElement("Pride and Prejudice", "Jane Austen", 279, "read");
+addElement("The Catcher in the Rye", "J.D. Salinger", 224, "read");
+addElement("Brave New World", "Aldous Huxley", 288, "read");
+addElement("The Lord of the Rings", "J.R.R. Tolkien", 1178, "read");
+addElement("Moby-Dick", "Herman Melville", 720, "read");
+addElement(
+  "Harry Potter and the Philosopher's Stone",
+  "J.K. Rowling",
+  223,
+  "read",
+);
+console.log(myLibrary);
