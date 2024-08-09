701fbeb9085fbc2ef69610c6dd6af9241086d932
Mateusz Karpinski <mateusz.karpinski29@gmail.com>
2024-08-09 13:33:45 +0000

    added read feature

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,4 @@
+
 const addBtn = document.querySelector("#addBtn");
 const allBtn = document.querySelector("#all");
 
@@ -19,64 +20,95 @@ function Book(title, author, pages, read) {
   };
 }
 
+// classes in arrays
+
+const divClasses = [
+  "element",
+  "border-2",
+  "rounded",
+  "p-6",
+  "flex",
+  "flex-col",
+  "border-black",
+  "justify-center",
+  "h-64",
+  "w-72",
+];
+const titleClasses = ["font-bold", "pb-3", "text-xl", "text-wrap", "basis-1/4"];
+const readClasses = [
+  "flex",
+  "justify-center",
+  "items-center",
+  "w-10",
+  "h-10",
+  "bg-green-400",
+  "custom-button",
+];
+const buttonClasses = [
+  "custom-button",
+  "place-self-end",
+  "bg-red-400",
+  "flex",
+  "justify-center",
+  "items-center",
+  "w-10",
+  "h-10",
+];
+const btnsParentClasses = ["flex", "justify-between", "pt-2"];
 // elements from html
 
 function addElement(sTitle, sAuthor, sPages, sRead) {
   myLibrary.push(new Book(sTitle, sAuthor, sPages, sRead));
   const main = document.querySelector("#main");
   const div = document.createElement("div");
-  const divClasses = [
-    "element",
-    "border-2",
-    "rounded",
-    "p-6",
-    "flex",
-    "flex-col",
-    "border-black",
-    "justify-center",
-    "h-60",
-    "w-72",
-  ];
-  addManyClasses(div, divClasses);
   const divTitle = document.createElement("h2");
-  const titleClasses = [
-    "font-bold",
-    "pb-3",
-    "text-xl",
-    "text-wrap",
-    "basis-1/4",
-  ];
-  addManyClasses(divTitle, titleClasses);
   const divAuthor = document.createElement("p");
   const divPages = document.createElement("p");
   const divStatus = document.createElement("p");
   const semibold = [divAuthor, divPages, divStatus];
+  const btnsParent = document.createElement("div");
+  const readButton = document.createElement("button");
+  const divButton = document.createElement("button");
+  addManyClasses(div, divClasses);
+  addManyClasses(divTitle, titleClasses);
+  addManyClasses(btnsParent, btnsParentClasses);
+  addManyClasses(readButton, readClasses);
+  if (sRead == "not read") {
+    readButton.classList.toggle("bg-red-500");
+    readButton.classList.toggle("bg-green-400");
+  }
+  addManyClasses(divButton, buttonClasses);
   semibold.forEach((tag) => {
     tag.classList.add("font-semibold");
   });
 
-  const divButton = document.createElement("button");
-  const buttonClasses = [
-    "custom-button",
-    "place-self-end",
-    "bg-red-300",
-    "flex",
-    "justify-center",
-    "items-center",
-    "w-10",
-    "h-10",
-  ];
-  addManyClasses(divButton, buttonClasses);
+  const divChildren = [divTitle, divAuthor, divPages, divStatus, btnsParent];
+  divChildren.forEach((child) => {
+    div.appendChild(child);
+  });
+  btnsParent.appendChild(readButton);
+  btnsParent.appendChild(divButton);
+  // buttons functions
   divButton.addEventListener("click", () => {
     let index = myLibrary.findIndex(
-      (book) => book.title == divButton.parentNode.firstChild.textContent,
+      (book) => book.title == div.firstChild.textContent,
     );
     if (index > -1) {
       myLibrary.splice(index, 1);
     }
-    divButton.parentNode.remove();
+    divButton.parentNode.parentNode.remove();
   });
-  const divChildren = [divTitle, divAuthor, divPages, divStatus, divButton];
+  readButton.addEventListener("click", () => {
+    if (div.children[3].textContent == "Status: read") {
+      readButton.classList.toggle("bg-red-500");
+      readButton.classList.toggle("bg-green-400");
+      divStatus.innerHTML = "Status: <span class='font-normal'>not read</span>";
+    } else {
+      readButton.classList.toggle("bg-red-500");
+      readButton.classList.toggle("bg-green-400");
+      divStatus.innerHTML = "Status: <span class='font-normal'>read</span>";
+    }
+  });
 
   // adding text to elements
   divTitle.textContent = sTitle;
@@ -85,10 +117,8 @@ function addElement(sTitle, sAuthor, sPages, sRead) {
   divPages.innerHTML = "Pages: <span class='font-normal'>" + sPages + "</span>";
   divStatus.innerHTML =
     "Status: <span class='font-normal'>" + sRead + "</span>";
+  readButton.textContent = "R";
   divButton.textContent = "D";
-  divChildren.forEach((child) => {
-    div.appendChild(child);
-  });
 
   main.appendChild(div);
 }
@@ -109,12 +139,6 @@ addElement("The Catcher in the Rye", "J.D. Salinger", 224, "read");
 addElement("Brave New World", "Aldous Huxley", 288, "read");
 addElement("The Lord of the Rings", "J.R.R. Tolkien", 1178, "read");
 addElement("Moby-Dick", "Herman Melville", 720, "read");
-addElement(
-  "Harry Potter and the Philosopher's Stone",
-  "J.K. Rowling",
-  223,
-  "read",
-);
 
 const addMenu = document.querySelector("#addMenu");
 const addBookBtn = document.querySelector("#addBookBtn");
